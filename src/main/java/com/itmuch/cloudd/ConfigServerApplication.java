e2501c758e925a3b2e2a45e24b4198dc8782aa99
'1932361772' <'1932361772@qq.com'>
2017-07-23 10:35:41 +0800

    check ConfigServerApplication

--- src/main/java/com/itmuch/cloudd/ConfigServerApplication.java
+++ src/main/java/com/itmuch/cloudd/ConfigServerApplication.java
@@ -2,11 +2,12 @@ package com.itmuch.cloudd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	@Bean
